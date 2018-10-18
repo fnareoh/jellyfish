f0b7bf8263bfe57263b1e3375d6ad32e007722db
fnareoh <garance.gourdel@laposte.net>
2018-10-18 09:52:07 +0200

    Add parameter for position of thejellyfish and squeezing

--- src/sample.cpp
+++ src/sample.cpp
@@ -9,36 +9,40 @@
 
 
 // d_theta and d_alpha are angles in radient.
-Mesh sample_half_sphere(double r, double d_theta, double d_alpha){
+Mesh sample_half_sphere(double pos_x, double pos_y, double pos_z,
+					 double r, double d_theta, double d_alpha,
+					 double squeeze){
 	Mesh mesh_half_sphere;
+	double nb_step_alpha = PI/(2*d_alpha);
 	for(int i=0; i*d_alpha < PI/2; i++){
 		for(int j=0; j*d_theta < 2*PI; j++){
 
 			double d_t = std::min(d_theta, 2*PI-j*d_theta);
 			double d_a = std::min(d_alpha, PI/2-i*d_alpha);
-
+			double r_comp_i = r*(1+ squeeze*((nb_step_alpha-i)/nb_step_alpha));
+			double r_comp_i_plus = r*(1+ squeeze*((nb_step_alpha-i-1)/nb_step_alpha));
 			Point3 a = {
-				r*cos(i*d_alpha)*cos(j*d_theta),
-				r*cos(i*d_alpha)*sin(j*d_theta),
-				r*sin(i*d_alpha)
+				pos_x + r_comp_i*cos(i*d_alpha)*cos(j*d_theta),
+				pos_y + r_comp_i*cos(i*d_alpha)*sin(j*d_theta),
+				pos_z + r_comp_i*sin(i*d_alpha)
 			};
 
 			Point3 b = {
-				r*cos(i*d_alpha)*cos(j*d_theta+d_t),
-				r*cos(i*d_alpha)*sin(j*d_theta+d_t),
-				r*sin(i*d_alpha)
+				pos_x + r_comp_i*cos(i*d_alpha)*cos(j*d_theta+d_t),
+				pos_y + r_comp_i*cos(i*d_alpha)*sin(j*d_theta+d_t),
+				pos_z + r_comp_i*sin(i*d_alpha)
 			};
 
 			Point3 c = {
-				r*cos(i*d_alpha+d_a)*cos(j*d_theta),
-				r*cos(i*d_alpha+d_a)*sin(j*d_theta),
-				r*sin(i*d_alpha+d_a)
+				pos_x + r_comp_i_plus*cos(i*d_alpha+d_a)*cos(j*d_theta),
+				pos_y + r_comp_i_plus*cos(i*d_alpha+d_a)*sin(j*d_theta),
+				pos_z + r_comp_i_plus*sin(i*d_alpha+d_a)
 			};
 
 			Point3 d = {
-				r*cos(i*d_alpha+d_a)*cos(j*d_theta+d_t),
-				r*cos(i*d_alpha+d_a)*sin(j*d_theta+d_t),
-				r*sin(i*d_alpha+d_a)
+				pos_x + r_comp_i_plus*cos(i*d_alpha+d_a)*cos(j*d_theta+d_t),
+				pos_y + r_comp_i_plus*cos(i*d_alpha+d_a)*sin(j*d_theta+d_t),
+				pos_z + r_comp_i_plus*sin(i*d_alpha+d_a)
 			};
 
 			mesh_half_sphere.insert(a,b,c);
@@ -83,7 +87,7 @@ Mesh simple_tentacle(double len, double width, double pos_x, double pos_y){
 }
 
 int main() {
-	povray_output_mesh(std::cout, sample_half_sphere(5, PI/100, PI/100));
+	povray_output_mesh(std::cout, sample_half_sphere(0,0,0,5, PI/50, PI/50, -0.2));
 	povray_output_mesh(std::cout, simple_tentacle(7, 1, 0, 0));
 	povray_output_mesh(std::cout, simple_tentacle(4, 0.5, 1, 1));
 	povray_output_mesh(std::cout, simple_tentacle(4, 0.5, -1, 1));
